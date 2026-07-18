f524ddcc386073a626d46cccfcfe61789874862c
mari@klim <100579299+MariaKlimenteva@users.noreply.github.com>
2026-07-18 16:00:09 +0300

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r) {
+            auto mid = l + (r - l)/2;
+            if (nums.at(mid) > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+            if (nums.at(mid) == target) return mid;
+        }
+        return -1;
+    }
+};
